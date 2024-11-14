5b041cd1f9e35940a0afd65732913b6854ebbbd7
Miron <mironnesvetaylov@gmail.com>
2024-11-14 15:26:51 +0300

    No json to html step anymore

--- transform.js
+++ transform.js
@@ -1,12 +1,5 @@
 
 
-function WriteToHtml(card, filePath) {
-  const htmlContent = `
-${card.Sentences}
-${card.Dictionary}`;
-  fs.writeFileSync(filePath, htmlContent, 'utf8');
-};
-
 async function exportToAnki(card) {
   const ankiConnectUrl = "http://localhost:8765";
   let req = {
@@ -18,8 +11,7 @@ async function exportToAnki(card) {
         modelName: "TranslateFromNativeToTarget",
         fields: card,
         options: {
-          allowDuplicate: false,
-          duplicateScope: this.ankiDeckName,
+          allowDuplicate: true,
           duplicateScopeOptions: {
             deckName: "1. EngVocabulary::TranslateItToLearnNewWordWithContext",
             checkChildren: false,
@@ -35,59 +27,9 @@ async function exportToAnki(card) {
   console.log(body);
 }
 
-const zip = (a, b) => a.map((k, i) => [k, b[i]]);
-
-function createCard(word, data) {
-  let card = { Word: word, Sentences: "", Dictionary: "" };
-
-  let sentences = zip(data.examples, data.translations).map(([ex, tr]) => {
-    return `
-  <div class="sentence">
-    <div class="example">${ex}</div>
-    <div class="translation">${tr}</div>
-  </div>`;
-  }).join("\n");
-
-  card.Sentences = `
-<div class="sentences">
-  ${sentences}
-</div>
-  `;
-
-  let definitions = data.definitions.map((item) => {
-    let defSentences = zip(item.examples, item.translations).map(([ex, trans]) => {
-      return `
-      <div class="definition-sentence">
-        <div class="example">${ex}</div>
-        <div class="translation">${trans}</div>
-      </div>`;
-    }).join("\n");
-
-    return `
-  <div class="definition-container">
-    <div class="definition">${item.definition}</div>
-    <div class="word-translate">${item.wordTranslate}</div>
-    <div class="definition-sentences-container">
-      ${defSentences}
-    </div>
-  </div>`;
-  }).join("\n");
-
-  let dictionary = `
-<div class="dictionary">
-${definitions}
-</div>`;
-
-  card.Dictionary = dictionary;
-
-  return card;
-}
-
 const fs = require('fs');
 
-const filePath = './prompt_res.json';
+const filePath = './prompt_res.html';
 const data = fs.readFileSync(filePath, 'utf8');
-const jsonData = JSON.parse(data);
-const card = createCard("persuade", jsonData);
-WriteToHtml(card, "./out.html")
-//exportToAnki(card);
+const card = {Dictionary: data, word: "persuade"}
+exportToAnki(card);
