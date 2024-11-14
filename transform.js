f7e9bf95d2ae8004a3b7539ed2234d13947a2c3e
Miron <mironnesvetaylov@gmail.com>
2024-11-14 13:28:21 +0300

    Fix formatting. (Ugly code but it works:))))

--- transform.js
+++ transform.js
@@ -4,51 +4,48 @@ function createCard(word, data) {
   let card = { Word: word, Examples: "", Translations: "", Dictionary: "" };
 
   let examples = data.examples.map((item) => {
-    return `<div class="example">${item}</div>`;
+    return `  <div class="example">${item}</div>`;
   }).join("\n");
 
   card.Examples = `
 <div class="examples">
-  ${examples}
+${examples}
 </div>
   `;
 
   let translations = data.translations.map((item) => {
-    return `<div class="translation">${item}</div>`;
+    return `  <div class="translation">${item}</div>`;
   }).join("\n");
 
   card.Translations = `
 <div class="translations">
-  ${translations}
+${translations}
 </div>
   `;
 
   let definitions = data.definitions.map((item) => {
     let defExamples = zip(item.examples, item.translations).map(([ex, trans]) => {
       return `
-<div class="definition-example">
-  <div class="example">${ex}</div>
-  <div class="translation">${trans}</div>
-</div>
-`;
+      <div class="definition-example">
+        <div class="example">${ex}</div>
+        <div class="translation">${trans}</div>
+      </div>`;
     }).join("\n");
 
     return `
-<div class="definition-container">
-  <div class="definition">${item.definition}</div>
-  <div class="word-translate">${item.wordTranslate}</div>
-  <div class="definition-examples-container">
-    ${defExamples}
-  </div>
-</div>
-`;
+  <div class="definition-container">
+    <div class="definition">${item.definition}</div>
+    <div class="word-translate">${item.wordTranslate}</div>
+    <div class="definition-examples-container">
+      ${defExamples}
+    </div>
+  </div>`;
   }).join("\n");
 
   let dictionary = `
 <div class="dictionary">
-  ${definitions}
-</div>
-  `;
+${definitions}
+</div>`;
 
   card.Dictionary = dictionary;
 
@@ -68,25 +65,9 @@ console.log(serialized);
 
 // Создание HTML-строки
 const htmlContent = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>${card.Word}</title>
-    <style>
-        body { font-family: Arial, sans-serif; }
-        .examples, .translations, .dictionary { margin: 20px 0; }
-        .example, .translation, .definition { margin: 5px 0; }
-    </style>
-</head>
-<body>
-    ${card.Word}
-    ${card.Examples}
-    ${card.Translations}
-    ${card.Dictionary}
-</body>
-</html>
+${card.Examples}
+${card.Translations}
+${card.Dictionary}
 `;
 // Запись HTML в файл
 const outputHtmlFilePath = './output_card.html'; // Укажите путь к выходному HTML файлу
