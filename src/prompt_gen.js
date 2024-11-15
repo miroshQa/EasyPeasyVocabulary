22e599d864db14fa5dd6f23ba65018093ff7d7d1
Miron <mironnesvetaylov@gmail.com>
2024-11-15 18:57:00 +0300

    Change prompt. Force AI to include only two most popular definition for this word!! I think this binary thing is very genius here, because it is impossible to learn 3-6 definitions only for one card. But 2 definition it is very intutive 1 or 0

--- src/prompt_gen.js
+++ src/prompt_gen.js
@@ -4,10 +4,9 @@ I need help with the {Target} word "{Word}".
 Please give me information about this word in the following HTML format:
 
 <div class="word-dictionary">
-  <div class="part-of-speech-meaning">
-    <div class="part-of-speech">The part of speech of the "{Word}" for which definitions will be given</div>
     <div class="definitions">
       <div class="definition">
+        <div class="part-of-speech">The part of speech of the "{Word}" for which definitions will be given</div>
         <div class="definition-text">Definition of the word in {Target}</div>
         <div class="word-translate">Translation of the word in {Native} based on the definition</div>
         <div class="synonymous">Synonyms (provide 2-3 if available)</div>
@@ -27,19 +26,15 @@ Please give me information about this word in the following HTML format:
       </div>
         <!-- Repeat the above block for other definitions -->
       </div>
-  </div>
-  <!-- Repeat the above block for other parts of speech -->
 </div>
 
 
 Important Notes:
 - Please provide the response in HTML format only, without any extra comments.
 - Highlight the word using the <strong></strong> HTML tag.
-- Include 2-3 popular definitions, with 2 examples for each definition. It's okay if there are fewer than 3 definitions.
+- Include 2 the MOST popular definitions, with 2 examples for each definition. It's okay if only one definition exists
 - The translation should only be the word's translation according to the definition, not the translation of the definition
 itself.
-- If a word has different parts of speech, don't forget to add corresponding data
-- Don't include very rare defitions or parts of speech for the word
 `;
 
 
